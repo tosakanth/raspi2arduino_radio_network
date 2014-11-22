50d32dab0763094995f149f69adcb853396354b8
Tosakanth Project <tosakun.meeting@gmail.com>
2014-11-22 18:58:17 +0700

    Update pi_demo.py

--- raspberry_pi/pi_demo.py
+++ raspberry_pi/pi_demo.py
@@ -1,1 +1,15 @@
 from nrf24_wrapper import *
+import time
+
+radio = P2PNetwork()
+radio.start()
+radio.showDetails()
+msg='1234567890'
+try:
+  while True:
+    radio.send_msg(msg)
+    reply=radio.get_msg()
+    print reply
+    time.sleep(0.5)
+except KeyboardInterrupt :
+  radio.close()
