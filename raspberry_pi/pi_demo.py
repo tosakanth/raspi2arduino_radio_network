b26080883795ffa1e7937d9c3f95ceffaee08f72
Tosakanth Project <tosakun.meeting@gmail.com>
2014-11-26 23:44:07 +0700

    Update pi_demo.py

--- raspberry_pi/pi_demo.py
+++ raspberry_pi/pi_demo.py
@@ -1,15 +1,41 @@
-from nrf24_wrapper import *
-import time
+from NRF24L01_wrapper import TK_RADIO
+from time import gmtime,strftime
 
-radio = P2PNetwork()
-radio.start()
-radio.showDetails()
-msg='I am RasPi'
+def parseData(data):
+	humid=''
+	temp=''
+	station=''
+	for i in range(5):
+		humid=humid+chr(data[i])
+	for i in range(5,10):
+		temp=temp+chr(data[i])
+	for i in range(10,15) :
+		station=station+chr(data[i])
+	print "Humidity : %s percent" % (humid)
+	print "Temperature : %s" % (temp)
+	print "From station : %s" % (station)
+	print strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+	print "=============================="			
+
+tk_radio = TK_RADIO()
+tk_radio.begin()
+readingPipes=[[0xF0, 0xF0, 0xF0, 0xF0, 0xC2],[0xF0, 0xF0, 0xF0, 0xF0, 0xC3],[0xF0, 0xF0, 0xF0, 0xF0, 0xC4],[0xF0, 0xF0, 0xF0, 0xF0, 0xC5]]
+tk_radio.openReadingPipes(readingPipes)
+tk_radio.showDetails()
+
+stop_flag=False
+
 try:
-  while True:
-    radio.send_msg(msg)
-    reply=radio.get_msg()
-    print reply
-    time.sleep(0.5)
-except KeyboardInterrupt :
-  radio.close()
+	while True:
+		recv_buf= []		
+		recv_buf=tk_radio.get_msg()		
+		if recv_buf != [] :
+			parseData(recv_buf)
+		#out = ''.join(chr(i) for i in recv_buf)
+		#if out != '':
+		#	print out
+   
+except KeyboardInterrupt:
+   tk_radio.close();
+
+
