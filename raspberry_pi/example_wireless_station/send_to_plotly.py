df0fc9f1368b4840c847bbc38c77c0812657f346
Tosakanth Project <tosakun.meeting@gmail.com>
2015-07-10 10:28:38 +0700

    Update send_to_plotly.py

--- raspberry_pi/example_wireless_station/send_to_plotly.py
+++ raspberry_pi/example_wireless_station/send_to_plotly.py
@@ -1,4 +1,4 @@
-from nrf24_wrapper import TK_RADIO
+from NRF24L01_wrapper import TK_RADIO
 from time import gmtime,strftime
 import plotly.plotly as py # plotly library
 from plotly.graph_objs import Scatter, Layout, Figure # plotly graph objects
